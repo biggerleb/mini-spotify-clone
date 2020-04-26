5f9e4db3432a5b258331b0efe1751371676737e3
biggerleb <marek.mucha99@gmail.com>
2020-04-26 13:20:50 +0200

    Updates

---
+++ precache-manifest.e2116f2244117bc10a830f22669e8981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14024a3f95c97ed9ca33",
+    "url": "/biggerleb/mini-spotify-clone/static/css/main.93fbc697.chunk.css"
+  },
+  {
+    "revision": "14024a3f95c97ed9ca33",
+    "url": "/biggerleb/mini-spotify-clone/static/js/main.14024a3f.chunk.js"
+  },
+  {
+    "revision": "1eaf59175903632c62fc",
+    "url": "/biggerleb/mini-spotify-clone/static/js/runtime~main.1eaf5917.js"
+  },
+  {
+    "revision": "ad8d7e10d6f473cd799c",
+    "url": "/biggerleb/mini-spotify-clone/static/js/2.ad8d7e10.chunk.js"
+  },
+  {
+    "revision": "401d6e66f5c4d7c8e4e5d29ab514d2a2",
+    "url": "/biggerleb/mini-spotify-clone/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/biggerleb/mini-spotify-clone/precache-manifest.e2116f2244117bc10a830f22669e8981.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/biggerleb/mini-spotify-clone/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
